479eb1607e339753bad9040cb37918ee197377c9
Gregor <gregor@redetech.com>
2023-12-04 12:37:42 -0800

    Update setup script to download librispeech models and build graph (as they were too large to commit to git)

--- egs/hoaloha/setup.sh
+++ egs/hoaloha/setup.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-# used to build the language graph.  Only done once.  I committed the built graph data, but 
-# included this for reference
+# used to build the language graph.  
+# Only done once.  
+# run from kaldi/egs/hoaloh in WSL in a docker container started like this...
 
-# run from kaldi/egs/hoaloh in a docker container started like this...
 # sudo docker run -it -v /mnt/c/Users/Gersh/Git/kaldi_hoaloha/egs/hoaloha:/opt/kaldi/egs/hoaloha kaldiasr/kaldi:latest bash
+
 # replace '/mnt/c/Users/Gersh/Git/kaldi_hoaloha/egs/hoaloha' with the path to your haloha dir in the kaldi/egs tree
 
+apt-get update
+apt install curl
+cd egs/hoaloha
+
+# use the wsj example to build the graph
 cd ../wsj/s5
-cp ../../hoaloha/*.gz .
+
+curl -O https://kaldi-asr.org/models/13/0013_librispeech_v1_chain.tar.gz
+curl -O https://kaldi-asr.org/models/13/0013_librispeech_v1_lm.tar.gz
+curl -O https://kaldi-asr.org/models/13/0013_librispeech_v1_extractor.tar.gz
 
 tar -xzf 0013_librispeech_v1_chain.tar.gz
 tar -xzf 0013_librispeech_v1_lm.tar.gz
@@ -20,6 +29,5 @@ export graph_dir=$dir/graph_tgsmall
 # build the decoding graph from the lang_test_tgsmall data
 utils/mkgraph.sh --self-loop-scale 1.0 --remove-oov data/lang_test_tgsmall $dir $graph_dir
 
+# copy everything back to the hoaloha dir
 cp -r ./exp ../../hoaloha/s5
-
-rm *.gz
